b3dfb0d7c1998ad012be0905cbc66ac52f02d5ab
Franck Blanchard <info@aztequemedia.com>
2015-11-03 22:03:44 -0500

    Correction d'une faute de frappe dans les commentaires du script build-readme.sh

--- build-readme.sh
+++ build-readme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Ce script fabrique le fichier README.md en
-# conatenant les fichiers descriptifs de chaque
+# concatenant les fichiers descriptifs de chaque
 # répertoire contenant les scripts, mais en commençant
 # par le fichier du projet à la racine du projet
 # Il suffirat d'ajouter une ligne par script par la suite
